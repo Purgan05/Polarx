775bf259cfbcff7ac54c449d2b6106bf6f966ddc
Purgan05 <ikerbaba05@gmail.com>
2026-06-18 22:51:41 -0600

    better code

--- audifonos.js
+++ audifonos.js
@@ -6,14 +6,14 @@
 // Para editar/agregar productos, modifica este archivo.
 
 const listaProductosAudifonos = [
-    { id: 201, categoria: 'audifonos', activo: true, orden: 1, agotado: false, nombre: "Audífonos 1", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" },
-    { id: 202, categoria: 'audifonos', activo: true, orden: 2, agotado: false, nombre: "Audífonos 2", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" },
-    { id: 203, categoria: 'audifonos', activo: true, orden: 3, agotado: false, nombre: "Audífonos 3", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" },
-    { id: 204, categoria: 'audifonos', activo: true, orden: 4, agotado: false, nombre: "Audífonos 4", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" },
-    { id: 205, categoria: 'audifonos', activo: true, orden: 5, agotado: false, nombre: "Audífonos 5", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" },
-    { id: 206, categoria: 'audifonos', activo: true, orden: 6, agotado: false, nombre: "Audífonos 6", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" },
-    { id: 207, categoria: 'audifonos', activo: true, orden: 7, agotado: false, nombre: "Audífonos 7", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" },
-    { id: 208, categoria: 'audifonos', activo: true, orden: 8, agotado: false, nombre: "Audífonos 8", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" },
-    { id: 209, categoria: 'audifonos', activo: true, orden: 9, agotado: false, nombre: "Audífonos 9", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" },
-    { id: 210, categoria: 'audifonos', activo: true, orden: 10, agotado: false, nombre: "Audífonos 10", precio: 0, descripcion: "Descripción breve del producto.", img: "img/audifonos/portada_audifonos.jpg" }
+    { id: 201, categoria: 'audifonos', activo: true, orden: 1, agotado: false, nombre: "Audífonos 1", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_201_1.jpg", "img/audifonos/producto_201_2.jpg"] },
+    { id: 202, categoria: 'audifonos', activo: true, orden: 2, agotado: false, nombre: "Audífonos 2", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_202_1.jpg", "img/audifonos/producto_202_2.jpg"] },
+    { id: 203, categoria: 'audifonos', activo: true, orden: 3, agotado: false, nombre: "Audífonos 3", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_203_1.jpg", "img/audifonos/producto_203_2.jpg"] },
+    { id: 204, categoria: 'audifonos', activo: true, orden: 4, agotado: false, nombre: "Audífonos 4", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_204_1.jpg", "img/audifonos/producto_204_2.jpg"] },
+    { id: 205, categoria: 'audifonos', activo: true, orden: 5, agotado: false, nombre: "Audífonos 5", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_205_1.jpg", "img/audifonos/producto_205_2.jpg"] },
+    { id: 206, categoria: 'audifonos', activo: true, orden: 6, agotado: false, nombre: "Audífonos 6", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_206_1.jpg", "img/audifonos/producto_206_2.jpg"] },
+    { id: 207, categoria: 'audifonos', activo: true, orden: 7, agotado: false, nombre: "Audífonos 7", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_207_1.jpg", "img/audifonos/producto_207_2.jpg"] },
+    { id: 208, categoria: 'audifonos', activo: true, orden: 8, agotado: false, nombre: "Audífonos 8", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_208_1.jpg", "img/audifonos/producto_208_2.jpg"] },
+    { id: 209, categoria: 'audifonos', activo: true, orden: 9, agotado: false, nombre: "Audífonos 9", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_209_1.jpg", "img/audifonos/producto_209_2.jpg"] },
+    { id: 210, categoria: 'audifonos', activo: true, orden: 10, agotado: false, nombre: "Audífonos 10", precio: 0, descripcion: "Descripción breve del producto.", imagenes: ["img/audifonos/producto_210_1.jpg", "img/audifonos/producto_210_2.jpg"] }
 ];
