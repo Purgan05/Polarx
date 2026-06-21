2f6d0e5eb9f4f8e9129b3f1eed9d9f4e6993daf1
Purgan05 <ikerbaba05@gmail.com>
2026-06-20 20:59:54 -0600

    img y prices

--- plumas.js
+++ plumas.js
@@ -8,25 +8,25 @@
 // ============================================================
 const listaProductosPlumas = [
     {
-        id: 401, nombre: 'Besos 2g', precio: 0,
+        id: 401, nombre: 'Besos 2g', precio: 999,
         descripcion: 'Pluma de 2g Sativa',
         imagenes: ['img/plumas/besos_2g_f1.webp', 'img/plumas/besos_2g_f2.webp'],
         categoria: 'plumas', activo: true, agotado: false, orden: 1
     },
     {
-        id: 402, nombre: 'Muhameds v2', precio: 0,
+        id: 402, nombre: 'Muhameds v2', precio: 1200,
         descripcion: 'Pluma de 2g Indica',
         imagenes: ['img/plumas/muhameds_v2_f1.webp', 'img/plumas/muhameds_v2_f2.webp'],
         categoria: 'plumas', activo: true, agotado: false, orden: 2
     },
     {
-        id: 403, nombre: 'Muhameds v3', precio: 0,
+        id: 403, nombre: 'Muhameds v3', precio: 1200,
         descripcion: 'Pluma de 2g Hibrida',
         imagenes: ['img/plumas/muhameds_v3_f1.webp', 'img/plumas/muhameds_v3_f2.webp'],
         categoria: 'plumas', activo: true, agotado: false, orden: 3
     },
     {
-        id: 404, nombre: 'Packman x Partyboyz', precio: 0,
+        id: 404, nombre: 'Packman x Partyboyz', precio: 1390,
         descripcion: 'Pluma de 3g + Pre roll',
         imagenes: ['img/plumas/packmanxpartyboyz_f1.webp', 'img/plumas/packmanxpartyboyz_f2.webp'],
         categoria: 'plumas', activo: true, agotado: false, orden: 4
